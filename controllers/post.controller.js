d1f048bf41aa021a099f1b089ecee8946de8ac65
d4rcel <daraola05@gmail.com>
2023-03-17 00:54:03 +0100

    Like and Unlike Post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -68,3 +68,57 @@ module.exports.deletePost = async (req, res) => {
     res.status(400).send(e);
   }
 };
+
+// Like And Unlike Post
+
+module.exports.likePost = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID Unknown : " + req.params.id);
+
+  try {
+    const likedPost = await PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $addToSet: { likers: req.body.id },
+      },
+      { new: true }
+    ).exec();
+
+    const liker = await UserModel.findByIdAndUpdate(
+      req.body.id,
+      {
+        $addToSet: { likes: req.params.id },
+      },
+      { new: true }
+    ).exec();
+    return res.status(200).json({ likedPost, liker });
+  } catch (e) {
+    return res.status(400).send(e);
+  }
+};
+
+module.exports.unlikePost = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID Unknown : " + req.params.id);
+
+    try {
+      const unlikedPost = await PostModel.findByIdAndUpdate(
+        req.params.id,
+        {
+          $pull: { likers: req.body.id },
+        },
+        { new: true }
+      ).exec();
+  
+      const unliker = await UserModel.findByIdAndUpdate(
+        req.body.id,
+        {
+          $pull: { likes: req.params.id },
+        },
+        { new: true }
+      ).exec();
+      return res.status(200).json({ unlikedPost, unliker });
+    } catch (e) {
+      return res.status(400).send(e);
+    }
+};
